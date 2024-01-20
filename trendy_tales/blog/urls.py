982723370969c149cff779711d07b8dc121c8e4f
Inamul Hassan <hassan.inamul@outlook.com>
2024-01-20 11:18:28 +0530

    Trendy Tales all posts and index html and static completed

--- trendy_tales/blog/urls.py
+++ trendy_tales/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="index-page"),
     path("posts", views.posts, name="all-posts"),
-    path("posts/<slug:slug>", views.single_post, name="post")
+    path("posts/<slug:slug>", views.single_post, name="single-post")
 ]
