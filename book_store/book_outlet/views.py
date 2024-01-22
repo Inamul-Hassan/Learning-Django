92a552bc07bce3a4813fc7eb92bd18964c2e2c70
Inamul Hassan <hassan.inamul@outlook.com>
2024-01-22 14:26:38 +0530

    Detailed Page with data connection is implemented

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from book_outlet.models import Book
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'book_outlet/index.html')
+    books = Book.objects.all()
+    Book.objects.filter(title__endswith='us').update(title='It Ends With Us')
+    return render(request, 'book_outlet/index.html', {'books': books})
+
+
+def detailed(request, id):
+    # try:
+    #     book = Book.objects.get(id=id)
+    # except:
+    #     Http404('Book not found')
+    book = get_object_or_404(Book, id=id)
+    return render(request, 'book_outlet/detailed.html',
+                  {'title': book.title,
+                   'rating': book.rating,
+                   'author': book.author,
+                   'is_bestseller': book.is_bestseller})
