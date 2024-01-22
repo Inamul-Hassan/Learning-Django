429dc610b2d330339ba323095a0aa6483db7c60f
Inamul Hassan <hassan.inamul@outlook.com>
2024-01-22 15:11:07 +0530

    Before Slugify

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,6 +10,14 @@ class Book(models.Model):
     rating = models.IntegerField()
     author = models.CharField(max_length=50, blank=True, null=True)
     is_bestseller = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def __str__(self) -> str:
         return f"{self.title} ({self.rating})"
+
+    def get_absolute_url(self):
+        return reverse("detailed", args=[self.id])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
