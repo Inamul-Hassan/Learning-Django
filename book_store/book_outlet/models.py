65336b6bb4a8680c068fb215e2e064f410221ec4
Inamul Hassan <hassan.inamul@outlook.com>
2024-01-22 13:01:12 +0530

    book_outlet templates created

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,5 +4,10 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=50)
     rating = models.IntegerField()
+    author = models.CharField(max_length=50, blank=True, null=True)
+    is_bestseller = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.rating})"
